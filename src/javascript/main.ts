3d707a3099db98b412f220d1325214dad80a18e4
Hinaser <rindo.hinase@gmail.com>
2017-02-25 15:55:05 +0900

    Added sidebar for menu

--- src/javascript/main.ts
+++ src/javascript/main.ts
@@ -2,9 +2,9 @@
 /// <reference path="../../node_modules/@types/jquery/index.d.ts" />
 
 import * as $ from 'jquery';
-(<any>window).$  = $;
+(<any>window).$  = $; // For debug purpose, expose jquery object to global window
 
-$(function(){
+function main(): any {
     // Navigation sidebar gimmick
     $('#toggle-nav').on('click', function(){
         let layout = $('.dpln-layout.top-header-left-nav-layout');
@@ -17,4 +17,11 @@ $(function(){
             layout.addClass('dpln-expand');
         }
     });
-});
+    
+    $('figure.collapsible figcaption').on('click', function(){
+        let target = $(this).next();
+        target.toggleClass('dpln-expand');
+    });
+}
+
+$(main);
