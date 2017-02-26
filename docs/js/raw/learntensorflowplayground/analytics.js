52c12eae5846532c86b1752bc577b4bf62b0b7ca
Hinaser <rindo.hinase@gmail.com>
2017-02-26 14:25:45 +0900

    Build for production

--- docs/js/raw/learntensorflowplayground/analytics.js
+++ docs/js/raw/learntensorflowplayground/analytics.js
@@ -1,8 +1,1 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-var ANALYTICS_ID = 'UA-91245492-1';
-ga('create', ANALYTICS_ID, 'auto');
-ga('send', 'pageview');
+!function(e,t,a,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=t.createElement(a),s=t.getElementsByTagName(a)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,"script","https://www.google-analytics.com/analytics.js","ga");var ANALYTICS_ID="UA-91245492-1";ga("create",ANALYTICS_ID,"auto"),ga("send","pageview");
