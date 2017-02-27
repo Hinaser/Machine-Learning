b739d042d8e1ba1eaa783feed5c7d8a78e964331
Hinaser <rindo.hinase@gmail.com>
2017-02-27 11:14:19 +0900

    Inner layout system change

--- src/javascript/dispatcher.ts
+++ src/javascript/dispatcher.ts
@@ -5,12 +5,19 @@ export default class Dispatcher {
     static initPageScript(): void {
         $(function(){
             let page = $('body > .dpln-layout > main > article').data('page');
+            Dispatcher.initMenu(page);
 
             switch(page){
-                case 'learntensorflowplayground':
+                case 'page-learntensorflowplayground':
                     new LearnTensorFlowPlayground();
                     break;
             }
         });
+    }
+
+    static initMenu(page_id: string): void {
+        let menu = $('#' + page_id);
+        menu.addClass('dpln-expand');
+        menu.find('figcaption button i').text('keyboard_arrow_up');
     }
 }
