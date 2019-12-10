8064914efaf5eb281f979b6c67c188cb9f2adeef
Hinase <rindo.hinase@gmail.com>
2019-12-10 14:56:22 +0900

    Upgrade npm modules (#21)
    * Upgraded gulp/vinyl
    
    * Upgraded browserify
    
    * Changed mathjax cdn location
    
    * Build the latest documents

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -21,158 +21,162 @@ var gutil = require('gulp-util');
 
 var config = require('../config.js');
 
-gulp.task('build:css', ['clean:css'], function(){
+gulp.task('build:css', gulp.series('clean:css', function(){
     return gulp.src(config['stylesheet']['srcDir'] + '/main.styl')
-        .pipe(plumber())
-        .on('error', function(err){
-            console.error(err.message);
-        })
-        .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.init()))
-        .pipe(stylus({
-            'compress': config['stylesheet']['compress'],
-            'include css': true
-        }))
-        .pipe(autoprefixer())
-        //.pipe(concat('main.css'))
-        .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.write()))
-        .pipe(gulp.dest(config['stylesheet']['destDir']))
-});
+      .pipe(plumber())
+      .on('error', function(err){
+          console.error(err.message);
+      })
+      .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.init()))
+      .pipe(stylus({
+          'compress': config['stylesheet']['compress'],
+          'include css': true
+      }))
+      .pipe(autoprefixer())
+      //.pipe(concat('main.css'))
+      .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.write()))
+      .pipe(gulp.dest(config['stylesheet']['destDir']))
+}));
 
-gulp.task('build:js', ['clean:js'], function(){
-    browserify({
+gulp.task('build:js', gulp.series('clean:js', function(){
+    return browserify({
         debug: config['js']['sourcemaps']
     })
-        .add(config['js']['srcDir'] + '/main.ts')
-        .plugin(tsify, config['js']['tsconfig'])
-        .bundle()
-        .on('error', function(err){
-            console.error(err.message);
-            this.emit('end');
-        })
-        .pipe(source('main.js'))
-        .pipe(buffer())
-        .pipe(gulpif(config['js']['compress'], uglify()))
-        .pipe(gulp.dest(config['js']['destDir']));
-});
+      .add(config['js']['srcDir'] + '/main.ts')
+      .plugin(tsify, config['js']['tsconfig'])
+      .bundle()
+      .on('error', function(err){
+          console.error(err.message);
+          this.emit('end');
+      })
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(gulpif(config['js']['compress'], uglify()))
+      .pipe(gulp.dest(config['js']['destDir']));
+}));
 
-gulp.task('build:image', /*['clean:image'],*/ function(){ // gulp-changed cannot be used with clean
+gulp.task('build:image', gulp.series(/*'clean:image',*/ function(){ // gulp-changed cannot be used with clean
     return gulp.src([config['image']['srcDir'] + '/**/*.{tiff,jpg,png,gif}'], {base: config['image']['srcDir']})
-        .pipe(plumber())
-        .pipe(newer(config['image']['destDir']))
-        .pipe(gulp.dest(config['image']['destDir']));
-});
+      .pipe(plumber())
+      .pipe(newer(config['image']['destDir']))
+      .pipe(gulp.dest(config['image']['destDir']));
+}));
 
-gulp.task('build:html', ['build:css', 'build:js', 'build:image', 'build:lib', 'build:raw', 'clean:html'], function(){
-    return gulp.src(config['html']['srcDir'] + '/*.pug')
-        .pipe(plumber())
-        .pipe(pug())
-        .pipe(uncache({
-            rename: false,
-            append: 'hash',
-            srcDir: config['html']['destDir']
-        }))
-        .pipe(gulp.dest(config['html']['destDir']))
-});
-
-gulp.task('build:html:only', ['build:css', 'build:image'], function(){
-    return gulp.src(config['html']['srcDir'] + '/*.pug')
-        .pipe(plumber())
-        .pipe(pug())
-        .pipe(uncache({
-            rename: false,
-            append: 'hash',
-            srcDir: config['html']['destDir']
-        }))
-        .pipe(gulp.dest(config['html']['destDir']))
-});
-
-gulp.task('build', ['build:html']);
-
-gulp.task('build:lib:js', /*['clean:lib:js'],*/ function(){ // clean:lib:js cannot be used with gulp-newer
+gulp.task('build:lib:js', gulp.series(/*'clean:lib:js',*/ function(){ // clean:lib:js cannot be used with gulp-newer
     return gulp.src(config['js']['libDir'] + '/*.js')
-        .pipe(plumber())
-        .pipe(newer(config['js']['destDir'] + '/lib.js'))
-        .pipe(gulpif(config['js']['sourcemaps'], sourcemaps.init()))
-        .pipe(concat('lib.js'))
-        .pipe(gulpif(config['js']['compress'], uglify()))
-        .pipe(gulpif(config['js']['sourcemaps'], sourcemaps.write()))
-        .pipe(gulp.dest(config['js']['destDir']));
-});
+      .pipe(plumber())
+      .pipe(newer(config['js']['destDir'] + '/lib.js'))
+      .pipe(gulpif(config['js']['sourcemaps'], sourcemaps.init()))
+      .pipe(concat('lib.js'))
+      .pipe(gulpif(config['js']['compress'], uglify()))
+      .pipe(gulpif(config['js']['sourcemaps'], sourcemaps.write()))
+      .pipe(gulp.dest(config['js']['destDir']));
+}));
 
-gulp.task('build:lib:css', /*['clean:lib:css'],*/ function() { // clean:lib:css cannot be used with gulp-newer
+gulp.task('build:lib:css', gulp.series(/*'clean:lib:css',*/ function() { // clean:lib:css cannot be used with gulp-newer
     return gulp.src(config['stylesheet']['libDir'] + '/*.css')
-        .pipe(plumber())
-        .pipe(newer(config['stylesheet']['destDir'] + '/lib.css'))
-        .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.init()))
-        .pipe(gulpif(config['stylesheet']['compress'], cleanCSS()))
-        .pipe(concat('lib.css'))
-        .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.write()))
-        .pipe(gulp.dest(config['stylesheet']['destDir']))
-});
+      .pipe(plumber())
+      .pipe(newer(config['stylesheet']['destDir'] + '/lib.css'))
+      .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.init()))
+      .pipe(gulpif(config['stylesheet']['compress'], cleanCSS()))
+      .pipe(concat('lib.css'))
+      .pipe(gulpif(config['stylesheet']['sourcemaps'], sourcemaps.write()))
+      .pipe(gulp.dest(config['stylesheet']['destDir']))
+}));
 
-gulp.task('build:lib', ['build:lib:js', 'build:lib:css']);
+gulp.task('build:lib', gulp.series('build:lib:js', 'build:lib:css'));
 
-gulp.task('build:css:raw', /*['clean:css:raw'],*/ function(){
+gulp.task('build:css:raw', gulp.series(/*'clean:css:raw',*/ function(){
     return gulp.src(config['stylesheet']['rawDir'] + '/**/*.css')
-        .pipe(plumber())
-        .pipe(newer(config['stylesheet']['destDir'] + '/raw'))
-        .pipe(gulpif(config['stylesheet']['compress'], cleanCSS()))
-        .pipe(gulp.dest(config['stylesheet']['destDir'] + '/raw'));
-});
+      .pipe(plumber())
+      .pipe(newer(config['stylesheet']['destDir'] + '/raw'))
+      .pipe(gulpif(config['stylesheet']['compress'], cleanCSS()))
+      .pipe(gulp.dest(config['stylesheet']['destDir'] + '/raw'));
+}));
 
-gulp.task('build:js:raw', /*['clean:js:raw'],*/ function(){
+gulp.task('build:js:raw', gulp.series(/*'clean:js:raw',*/ function(){
     return gulp.src(config['js']['rawDir'] + '/**/*.js')
-        .pipe(plumber())
-        .pipe(newer(config['js']['destDir'] + '/raw'))
-        .pipe(gulpif(config['js']['compress'], uglify()))
-        .pipe(gulp.dest(config['js']['destDir'] + '/raw'));
-});
+      .pipe(plumber())
+      .pipe(newer(config['js']['destDir'] + '/raw'))
+      .pipe(gulpif(config['js']['compress'], uglify()))
+      .pipe(gulp.dest(config['js']['destDir'] + '/raw'));
+}));
 
-gulp.task('build:html:raw', ['clean:html:raw'], function(){
+gulp.task('build:html:raw', gulp.series('clean:html:raw', function(){
     return gulp.src(config['html']['rawDir'] + '/**/*.html')
-        .pipe(plumber())
-        .pipe(gulp.dest(config['html']['destDir'] + '/raw'));
-});
+      .pipe(plumber())
+      .pipe(gulp.dest(config['html']['destDir'] + '/raw'));
+}));
 
-gulp.task('build:raw', ['build:js:raw', 'build:css:raw', 'build:html:raw']);
+gulp.task('build:raw', gulp.series('build:js:raw', 'build:css:raw', 'build:html:raw'));
 
-gulp.task('watch-js', ['build:js'], function(){
+gulp.task('build:html', gulp.series('build:css', 'build:js', 'build:image', 'build:lib', 'build:raw', 'clean:html', function(){
+    return gulp.src(config['html']['srcDir'] + '/*.pug')
+      .pipe(plumber())
+      .pipe(pug())
+      .pipe(uncache({
+          rename: false,
+          append: 'hash',
+          srcDir: config['html']['destDir']
+      }))
+      .pipe(gulp.dest(config['html']['destDir']))
+}));
+
+gulp.task('build:html:only', gulp.series('build:css', 'build:image', function(){
+    return gulp.src(config['html']['srcDir'] + '/*.pug')
+      .pipe(plumber())
+      .pipe(pug())
+      .pipe(uncache({
+          rename: false,
+          append: 'hash',
+          srcDir: config['html']['destDir']
+      }))
+      .pipe(gulp.dest(config['html']['destDir']))
+}));
+
+gulp.task('build', gulp.series('build:html'));
+
+gulp.task('watch-js', gulp.series('build:js', function(){
     gulp.watch(config['js']['srcDir'] + '/**/*.ts', ['build:js']);
-});
+}));
 
-gulp.task('watch-css', ['build:css'], function(){
+gulp.task('watch-css', gulp.series('build:css', function(){
     gulp.watch(config['stylesheet']['srcDir'] + '/**/*.styl', ['build:css']);
-});
+}));
 
-gulp.task('watch-html', ['build:html'], function(){
+gulp.task('watch-html', gulp.series('build:html', function(){
     gulp.watch(config['html']['srcDir'] + '/**/*.pug', ['build:html']);
-});
+}));
 
-gulp.task('watch', ['watch-js', 'watch-css', 'watch-html']);
+gulp.task('watch', gulp.series('watch-js', 'watch-css', 'watch-html'));
 
-gulp.task('build:html:sync', ['build:html'], function(){browsersync.reload();});
-gulp.task('build:html:only:sync', ['build:html:only'], function(){browsersync.reload();});
+gulp.task('build:html:sync', gulp.series('build:html', function(){browsersync.reload();}));
+gulp.task('build:html:only:sync', gulp.series('build:html:only', function(){browsersync.reload();}));
 
-gulp.task('serve:dev', ['build'], function(){
+gulp.task('serve:dev', gulp.series('build', function(done){
     browsersync.init({
         server: {
             baseDir: './docs'
         }
     });
-
-    gulp.watch(config['js']['srcDir'] + '/**/*.ts', ['build:html:sync']);
-    gulp.watch(config['stylesheet']['srcDir'] + '/**/*.styl', ['build:html:sync']);
-    gulp.watch(config['html']['srcDir'] + '/**/*.pug', ['build:html:sync']);
-});
+    done();
+}, function(done){
+  gulp.watch(config['js']['srcDir'] + '/**/*.ts', gulp.series('build:html:sync'));
+  gulp.watch(config['stylesheet']['srcDir'] + '/**/*.styl', gulp.series('build:html:sync'));
+  gulp.watch(config['html']['srcDir'] + '/**/*.pug', gulp.series('build:html:sync'));
+  done();
+}));
 
 // Only watches/compiles .pug files
-gulp.task('serve:dev:html', ['build:html:only'], function(){
+gulp.task('serve:dev:html', gulp.series('build:html:only', function(done){
     browsersync.init({
         server: {
             baseDir: './docs'
         }
     });
-
-    gulp.watch(config['html']['srcDir'] + '/**/*.pug', ['build:html:only:sync']);
-    gulp.watch(config['stylesheet']['srcDir'] + '/**/*.styl', ['build:html:only:sync']);
-});
+  done();
+}, function(done){
+  gulp.watch(config['html']['srcDir'] + '/**/*.pug', gulp.series('build:html:only:sync'));
+  gulp.watch(config['stylesheet']['srcDir'] + '/**/*.styl', gulp.series('build:html:only:sync'));
+  done();
+}));
